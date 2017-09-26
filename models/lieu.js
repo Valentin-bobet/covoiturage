e41b974ed09d3786e3698aadc3276ce0b46c55a8
Bobey <bobey.valentin@gmail.com>
2017-09-26 15:24:45 +0200

    Model Lieu + Test

---
+++ models/lieu.js
@@ -0,0 +1,16 @@
+class Lieu {
+    
+    constructor(id, libelle){
+        this._id = id;
+        this._libelle = libelle;
+    }
+    
+    get libelle(){
+        return this._libelle;
+    }
+    
+    toString(){
+        return 'Lieu : '+ this._libelle;
+    }
+}
+module.exports = Lieu;
