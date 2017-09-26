658fc99d24db80e45bf70b8298f3c7284c2c56a1
Bobey <bobey.valentin@gmail.com>
2017-09-26 15:13:41 +0200

    Gestion des offres, définition des routes

--- controllers/offreControlleur.js
+++ controllers/offreControlleur.js
@@ -1,3 +1,11 @@
 exports.offre_list = function(req, res, next) {
     res.send('NOT IMPLEMENTED: Offres list');
 };
+
+exports.offre_detail = function (req, res, next) {
+    re.send('NOT IMPLEMENTED: Detail Offre id : ' + req.params.id);
+};
+
+exports.offre_ajout_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Offre create POST');
+};
